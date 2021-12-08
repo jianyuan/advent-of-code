bfb68254a828fa2b40384b901c31dab7dce4ef5f
Jian Yuan Lee <jianyuan@gmail.com>
2021-12-08 23:45:50 +0000

    Clojure/2021/Day 3/Part 2

--- 2021/clojure/src/day03.clj
+++ 2021/clojure/src/day03.clj
@@ -2,31 +2,54 @@
   (:require [clojure.string :as str]
             [utils :refer [parse-integer]]))
 
-
 (def input
   (->> "data/day03.txt"
        slurp
-       str/split-lines
-       (apply mapv vector)))
+       str/split-lines))
 
-(defn frequencies-by [pred xs]
-  (->> xs
-       frequencies
-       (sort-by val pred)
-       first
-       key))
+(defn frequencies-by [xs pred]
+  (let [transposed (apply mapv vector xs)
+        n (/ (count xs) 2)]
+    (mapv (fn [ys]
+            (let [freq (->> ys
+                            frequencies
+                            (sort-by val pred)
+                            first)]
+              (if (= n (val freq))
+                (if (= pred >)
+                  \1
+                  \0)
+                (key freq))))
+          transposed)))
 
 (defn gamma-rate [xs]
-  (->> xs
-       (map #(frequencies-by > %))
-       (parse-integer 2)))
+  (parse-integer 2 (frequencies-by xs >)))
 
 (defn epsilon-rate [xs]
-  (->> xs
-       (map #(frequencies-by < %))
-       (parse-integer 2)))
+  (parse-integer 2 (frequencies-by xs <)))
+
+(defn generator-rating [xs pred]
+  (parse-integer 2
+                 (loop [ys xs
+                        index 0]
+                   (let [n (nth (frequencies-by ys pred) index)
+                         zs (filter #(= n (nth % index)) ys)]
+                     (if (= 1 (count zs))
+                       (first zs)
+                       (recur zs (inc index)))))))
+
+(defn oxygen-generator-rating [xs]
+  (generator-rating xs >))
+
+(defn co2-generator-rating [xs]
+  (generator-rating xs <))
 
 (defn part1 []
   (* (gamma-rate input) (epsilon-rate input)))
 
-(do (println "Part 1:" (part1)))
+(defn part2 []
+  (* (oxygen-generator-rating input) (co2-generator-rating input)))
+
+(do
+  (println "Part 1:" (part1))
+  (println "Part 2:" (part2)))
